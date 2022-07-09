ac9d9fa68e67ed0fd57098c05112f87b55c7ea52
rohos119 <37989815+rohos119@users.noreply.github.com>
2022-07-09 22:49:09 +0900

    Time: 1649 ms (43.56%), Space: 28.1 MB (47.64%) - LeetHub

---
+++ 1696-jump-game-vi/1696-jump-game-vi.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maxResult(self, nums: List[int], k: int) -> int:
+        dp = deque([(nums[0], 0)])
+        for i in range(1, len(nums)):
+            # index에 대한 조사?
+            # dp[0][1]+k 는 0+56
+            while dp and dp[0][1] + k < i:
+                dp.popleft()
+            cost = nums[i] + dp[0][0]
+            while dp and cost >= dp[-1][0]:
+                dp.pop()
+            dp.append((cost, i))
+        return dp[-1][0]
+        
