57db4ca8adbc4a8cc63eac5cffc62a8461217335
rohos119 <37989815+rohos119@users.noreply.github.com>
2022-08-06 03:12:38 +0900

    Time: 107 ms (5.46%), Space: 14.5 MB (7.70%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.py
@@ -6,48 +6,57 @@ class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
         # logic
         # target이 안에 있는지 어케 찾을래 ?
+        
         # easy way ->  O(M*N)
         # row한개씩 돌면서 만약에 사이값일때 if in 하면 true, 아니면 for문 진행
         # 만약 다돌았는데 없으면 ? false
-        j, end = 0, len(matrix[0])-1
-        while j < len(matrix):
-            if target <= matrix[j][end]:
-                if target in matrix[j]:
-                    return True
-                else:
-                    return False
-            else:
-                j+=1
-        return False
+        # j, end = 0, len(matrix[0])-1
+        # while j < len(matrix):
+        #     if target <= matrix[j][end]:
+        #         if target in matrix[j]:
+        #             return True
+        #         else:
+        #             return False
+        #     else:
+        #         j+=1
+        # return False
+        # ----통과----그러나 효율성이 떨어짐
+        
         # efficent way -> O(logm+logn) 
         # 시작점을 찾는거 -> logm / row 기준 바이너리 서치
         # 시작행에서 찾는거 -> logn / 행의 요소들로 바이너리 서치
         
-        # # find start point 
-        # m,n = len(matrix), len(matrix[0])
-        # rp1,rp2 = 0,m-1
-        # mid = (rp1+rp2)//2
-        # if rp1 == rp2 :
-        #     if target in matrix[rp1] :
-        #         return True
-        #     else :
-        #         return False
-        # while rp1 < rp2 :
-        #     mid = (rp1+rp2)//2
-        #     # 중간값에서 있으면 바로 종료
-        #     if target in matrix[mid] :
-        #         return True
-        #     # 중간값에 없으면
-        #     else :
-        #         if n == 1 :
-        #             if matrix[mid][0] < target :
-        #                 rp1 =mid
-        #             elif matrix[mid]
-        #         else :
-        #             if matrix[mid][-1] < target :
-        #                 rp1 = mid
-        #             elif matrix[mid][0] > target :
-        #                 rp2 = mid
-        #             else :
-        #                 return False
-        # return False
+        # 예외처리 
+        if target < matrix[0][0] and target > matrix[-1][-1]:
+            return False
+        
+        # find start row
+        m,n = len(matrix), len(matrix[0])
+        rp1,rp2 = 0,m-1
+        mid = (rp1+rp2)//2
+        while(rp1 <= rp2):
+            mid = (rp1 + rp2)//2
+            if (matrix[mid][0] <= target and target <= matrix[mid][n-1]):
+                rp1 = rp2 = mid
+                break
+            elif (target < matrix[mid][0]):
+                rp2 = mid - 1
+            else:
+                rp1 = mid + 1
+                
+        if rp1==rp2 :
+            if target in set(matrix[rp1]) :
+                return True
+        elif rp1<0 and matrix[rp1][0] <= target and target <= matrix[rp1][n-1] :
+            if target in set(matrix[rp1]) :
+                return True
+            else :
+                return False
+        elif rp2<m and matrix[rp2][0] <= target and target <= matrix[rp2][n-1] :
+            if target in set(matrix[rp2]) :
+                return True
+            else :
+                return False
+        else :
+            return False
+            
