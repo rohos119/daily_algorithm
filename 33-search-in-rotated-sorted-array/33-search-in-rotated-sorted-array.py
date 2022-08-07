d71507233c39e3320cee8dda64f13aa3859d091e
rohos119 <37989815+rohos119@users.noreply.github.com>
2022-08-07 23:52:13 +0900

    Time: 81 ms (19.64%), Space: 14.3 MB (18.83%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
@@ -3,6 +3,22 @@
 # input : 1 nums 5000 , -1e4 num[i] 1e4 nums[i]는 고유한 값
 # output : target의 index 없으면 -1
 class Solution:
-    def search(self, nums: List[int], target: int) -> int:
-        print(nums)
-        return nums.index(target) if target in nums else -1  
+	def search(self, nums: List[int], target: int) -> int:
+		left, right = 0, len(nums)-1
+		while left <= right:
+			mid = left + (right-left)//2
+			if nums[mid] == target:
+				return mid
+
+			if nums[left] <= nums[mid]:
+				if nums[mid] > target >= nums[left]:
+					right = mid - 1
+				else:
+					left = mid + 1
+			else:
+				if nums[mid] < target <= nums[right]:
+					left = mid + 1
+				else:
+					right = mid - 1
+
+		return -1
