ce0c22604951a810a0067313c29fa89f75cc29d5
rohos119 <37989815+rohos119@users.noreply.github.com>
2022-08-09 15:20:23 +0900

    Time: 178 ms (39.69%), Space: 15.5 MB (71.43%) - LeetHub

---
+++ 210-course-schedule-ii/210-course-schedule-ii.py
@@ -0,0 +1,39 @@
+# start 1049
+# 문제정의 : 코스를 완벽하게 끝내라, 코스정보는 prerequisties안에 있고 [next, frist] 이다
+# input : 1 numCourse 2000 , 0 prerequisites num*num-1 , a != b
+# output : course 순서 list / 없으면 empty array [0]
+from collections import *
+
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        
+        # logic 
+        # DFS로 풀자/ BFS 도 괜춘
+        # 일단 b 기준으로 sort하자, sort가 되있나?
+        graph = {i:[] for i in range(numCourses)}
+        edge = [0]*numCourses
+        for p in prerequisites :
+            graph[p[1]].append(p[0])
+            edge[p[0]] += 1
+        
+
+        pre = deque([i for i in range(numCourses) if edge[i] == 0])
+        answer = []
+        while pre :
+            p = pre.popleft()
+            answer.append(p)
+            for g in graph[p] :
+                # edge만큼 감소를 한다
+                edge[g] -= 1
+                # edge의 수만큼 감소해야 통과한것이므로
+                # edge가 0이 될때는 통과 -> que에 들어간다 -> 다음으로 넘어간다의 의미
+                if edge[g] == 0 :
+                    pre.append(g)       
+        
+        if len(answer) != numCourses :
+            return []
+        
+        return answer
+        
+        
+        
