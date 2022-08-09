979c47aab30f2fca0e0ea8a391462746c7ef1d5b
rohos119 <37989815+rohos119@users.noreply.github.com>
2022-08-09 15:42:48 +0900

    Time: 149 ms (61.93%), Space: 15.6 MB (71.43%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.py
+++ 210-course-schedule-ii/210-course-schedule-ii.py
@@ -10,29 +10,38 @@ class Solution:
         # logic 
         # DFS로 풀자/ BFS 도 괜춘
         # 일단 b 기준으로 sort하자, sort가 되있나?
+        
+        # BFS풀이
         graph = {i:[] for i in range(numCourses)}
         edge = [0]*numCourses
         for p in prerequisites :
             graph[p[1]].append(p[0])
             edge[p[0]] += 1
         
+        def bfs(edge,graph):
+            pre = deque([i for i in range(numCourses) if edge[i] == 0])
+            answer = []
+            while pre :
+                p = pre.popleft()
+                answer.append(p)
+                for g in graph[p] :
+                    # edge만큼 감소를 한다
+                    edge[g] -= 1
+                    # edge의 수만큼 감소해야 통과한것이므로
+                    # edge가 0이 될때는 통과 -> que에 들어간다 -> 다음으로 넘어간다의 의미
+                    if edge[g] == 0 :
+                        pre.append(g)       
 
-        pre = deque([i for i in range(numCourses) if edge[i] == 0])
-        answer = []
-        while pre :
-            p = pre.popleft()
-            answer.append(p)
-            for g in graph[p] :
-                # edge만큼 감소를 한다
-                edge[g] -= 1
-                # edge의 수만큼 감소해야 통과한것이므로
-                # edge가 0이 될때는 통과 -> que에 들어간다 -> 다음으로 넘어간다의 의미
-                if edge[g] == 0 :
-                    pre.append(g)       
+            if len(answer) != numCourses :
+                return []
+            return answer
         
-        if len(answer) != numCourses :
-            return []
-        return answer
+        
+        return bfs(edge,graph)
+    
+        # DFS풀이
+        
+        
         
         
         
