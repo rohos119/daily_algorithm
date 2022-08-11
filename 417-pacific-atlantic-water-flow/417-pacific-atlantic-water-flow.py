de88765be9a2bc04d569df9aeb1dbe7640ec3f69
rohos119 <37989815+rohos119@users.noreply.github.com>
2022-08-11 22:43:46 +0900

    Time: 611 ms (19.30%), Space: 17.5 MB (8.83%) - LeetHub

---
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.py
@@ -0,0 +1,41 @@
+
+# 문제정의 : 높이값이 주어진 m*n 에서, 물이 양옆에 
+class Solution:
+    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        pac, atl = set(), set()
+        ans = []
+        
+        for c in range(len(heights[0])):
+            self.dfs(0, c, heights, pac)
+            self.dfs(len(heights) - 1, c, heights, atl)
+        
+        
+        for r in range(len(heights)):
+            self.dfs(r, 0, heights, pac)
+            self.dfs(r, len(heights[0]) - 1, heights, atl)
+        
+            
+        for i in range(len(heights)):
+            
+            for j in range(len(heights[0])):
+                
+                if (i, j) in pac and (i, j) in atl:
+                    ans.append([i, j])
+        return ans
+        
+    def dfs(self, i, j, heights, pac):
+        
+        pac.add((i, j))
+        
+        direc = [(-1, 0), (0, -1), (0, 1), (1, 0)]
+        
+        for di in direc:
+            
+            r = i + di[0]
+            c = j + di[1]
+            
+            if r >= 0 and c >= 0 and r <= len(heights) -1 and c <= len(heights[0]) - 1:
+                
+                if heights[r][c] >= heights[i][j] and (r, c) not in pac:
+                    self.dfs(r, c, heights, pac)
+        
