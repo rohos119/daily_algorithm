4dc41c7a10dc080ac81f2cad397189e20f1990db
rohos119 <37989815+rohos119@users.noreply.github.com>
2022-08-16 23:37:44 +0900

    Create programers_괄호회전하기.py

---
+++ programers_괄호회전하기.py
@@ -0,0 +1,29 @@
+# start 1100
+# 문제정의 : 괄호를 회전하라
+def solution(s):
+    answer = 0
+    temp = list(s)
+    
+    for _ in range(len(s)):
+ 
+        st = []
+        for i in range(len(temp)):
+            if len(st) > 0:
+                if st[-1] == '[' and temp[i] == ']':
+                    st.pop()
+                elif st[-1] == '(' and temp[i] == ')':
+                    st.pop()
+                elif st[-1] == '{' and temp[i] == '}':
+                    st.pop()
+                else:
+                    st.append(temp[i])
+            else:
+                st.append(temp[i])
+        if len(st) == 0:
+            answer += 1
+        temp.append(temp.pop(0))
+ 
+    return answer
+ 
+ 
+ 
