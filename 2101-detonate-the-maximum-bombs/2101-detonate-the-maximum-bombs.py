ff78fac9ebc28aecbaf37e110cca751265d7512c
Beom seok noh <37989815+rohos119@users.noreply.github.com>
2022-09-05 00:36:15 +0900

    Time: 4928 ms (12.49%), Space: 14 MB (98.66%) - LeetHub

--- 2101-detonate-the-maximum-bombs/2101-detonate-the-maximum-bombs.py
+++ 2101-detonate-the-maximum-bombs/2101-detonate-the-maximum-bombs.py
@@ -5,52 +5,28 @@
 
 from collections import deque
 class Solution:
+    def dist(self, x, y):
+        return sqrt((x[0]-y[0])*(x[0]-y[0]) + (x[1]-y[1])*(x[1]-y[1]))
+    
     def maximumDetonation(self, bombs: List[List[int]]) -> int:
         # logic
         # 원의 범위 안에 있다는 것을 판단을 x2+y2 <= r2 으로 정하면 가능하다.
+        # 즉, 두 원점 사이의 거리가 r 안에 있으면 가능
         # 그렇다면 que 안에 어떻게 넣어야할까..?
-        
-        N = len(bombs)
-        
-        # step-1. build graph
-        graph = collections.defaultdict(list) # list not set
-        for i in range(N):
-            xi, yi, ri = bombs[i]
-            for j in range(N):
-                if i == j:
-                    continue
-                xj, yj, rj = bombs[j]
-                if (xj - xi) ** 2 + (yj - yi) ** 2 <= ri ** 2:
-                    graph[i].append(j)
-                    
-        # step-2. bfs recursive
-        # def bfs(start, visited):
-        #     visited.add(start)
-        #     for j in graph[start]:
-        #         if j not in visited:
-        #             bfs(j, visited)
-        
-        # step-2 bfs iterative
-        def bfs(start, visited=None):
-            if visited is None:
-                visited = set()
-            q = collections.deque([start])
-            cnt = 0
-            while q:
-                i = q.popleft()
-                if i not in visited:
-                    cnt += 1
-                    visited.add(i)
-                    for j in graph[i]:
-                        if j not in visited:
-                            q.append(j)
-            return cnt
-            
-        
-        # 3. get res
-        res = 0
-        for i in range(N):
+        res = 1
+        for i in range(len(bombs)):
+            current_res = 1
             visited = set()
-            bfs(i, visited)
-            res = max(res, len(visited))
+            visited.add(i)
+            q = collections.deque([i])
+            while q:
+                node = q.popleft()
+                for c in range(len(bombs)):
+                    if c not in visited and self.dist(bombs[node], bombs[c]) <= bombs[node][2]:
+                        q.append(c)
+                        visited.add(c)
+                        current_res += 1
+                        
+            res = max(res, current_res)
+                
         return res
